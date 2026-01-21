34225f2348caca2d4cc5a520b5437e92d5df212c
wazeveve <gabrielveanancio1123@gmail.com>
2026-01-20 23:31:41 -0300

    feat: Introduction in the basic Math Operations

--- HelloWorld/BasicOperations/Addition.cs
+++ HelloWorld/BasicOperations/Addition.cs
@@ -1,11 +1,11 @@
-﻿int firstNumber = 12;
-int secondNumber = 7;
+﻿//int firstNumber = 12;
+//int secondNumber = 7;
 
-Console.WriteLine(firstNumber + secondNumber);
+//Console.WriteLine(firstNumber + secondNumber);
 
 // What if we Mix the data types to force implicit type conversions
 
-string firstName = "Bob";
-int widgetsSold = 7;
+//string firstName = "Bob";
+//int widgetsSold = 7;
 
-Console.WriteLine(firstName + " sold " + ( widgetsSold + 7 ) + " widgets.");
+//Console.WriteLine(firstName + " sold " + ( widgetsSold + 7 ) + " widgets.");
---
+++ HelloWorld/BasicOperations/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+
+decimal decimalQuotient = 7.0m / 5;
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+int first = 7;
+int second = 5;
+decimal castQuotient = (decimal)first / (decimal)second;
+Console.WriteLine(castQuotient);
+
+
+Console.WriteLine($"Modulus of 200/5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7/5 : {7 % 5}");
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);

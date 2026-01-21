e0c758ee06673011623620121bfd75aa9899f036
wazeveve <gabrielveanancio1123@gmail.com>
2026-01-20 23:03:20 -0300

    feat: Solving the callenge proposed.

---
+++ HelloWorld/Challenges/ChallengeOne.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,7 +1,8 @@
 ﻿// Console.WriteLine("Hello, World!");
 
 // Difference between Write and WriteLine.
-Console.WriteLine("Congratualations!");
-Console.Write("You wrote your first lines of code.");
+//Console.WriteLine("Congratualations!");
+//Console.Write("You wrote your first lines of code.");
 
+// Only one compilation unit can have top-level statements. Thats why the code is commented.
 

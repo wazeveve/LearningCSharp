7513b6648165b25330f408428bb54923e647380d
wazeveve <gabrielveanancio1123@gmail.com>
2026-01-22 11:34:56 -0300

    feat: Store and Retrieve data using literal and variable values

--- HelloWorld/Challenges/ChallengeTwo.cs
+++ HelloWorld/Challenges/ChallengeTwo.cs
@@ -1,9 +1,12 @@
 ﻿// Fahrenheit to Celsius
 // Conversion formula: (F - 32) * 5/9 = C
 
+/*
 int fahrenheit = 94;
 fahrenheit -= 32;
 fahrenheit *= 5;
 decimal celsius = fahrenheit / 9m;
 
 Console.WriteLine("Celsius: " + celsius);
+
+*/
---
+++ HelloWorld/StoreAndRetrieveData/Exercise.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+// Variables
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+string name = "Bob";
+int messages  = 3;
+decimal temp = 34.4m;
+
+Console.WriteLine($"Hello, {name}! You have {messages} messages in your inbox. The temperature is {temp} celsius.");
